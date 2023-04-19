68071a7802d263701fc4fff3b0e5c17399e8dd10
Franka-T <77068560+Franka-T@users.noreply.github.com>
2023-04-19 13:43:42 +0200

    #121 add new construction mode (#136)
    * #121 add new construction mode 
    
    * to activate set const constructionMode=true in index.js line 94

--- app/index.js
+++ app/index.js
@@ -90,6 +90,14 @@ new mediator.TerminationConditionModelerHook(terminationConditionModeler);
 const errorBar = new ErrorBar(document.getElementById("errorBar"), mediator);
 const checker = new Checker(mediator, errorBar);
 
+// construction Mode for User Study, to enable set constructionMode to true
+const constructionMode = false;
+
+const titleObjectiveModel = document.getElementById("titleObjectiveModel");
+if (constructionMode) {titleObjectiveModel.innerHTML = "Milestone"} else titleObjectiveModel.innerHTML = "Objective Model";
+
+const titleDependencyModel = document.getElementById("titleDependencyModel");
+if (constructionMode) {titleDependencyModel.innerHTML = "Timeline"} else titleDependencyModel.innerHTML = "Dependency Model";
 
 async function loadDebugData() {
   const zip = new Zip();
